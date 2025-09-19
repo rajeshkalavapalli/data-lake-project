44d2612ec486b60015f287323c4925dd65f39e22
rajesh <rajesh.kalavapalli>
2025-09-19 15:53:36 +0530

    data-lake-firstfile

---
+++ scripts/generate.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import pandas as pd
+
+import random 
+import uuid
+from datetime import datetime, timedelta
+
+# generate users 
+
+users = []
+
+for i in range(1,101):
+    users.append(
+        {
+            'user_id':i,
+            'name':f'user_{i}',
+            'country': random.choice(['UK','US','GM','DE','FR'])
+        }
+    )
+
+
+df_users = pd.DataFrame(users)
+df_users.to_csv(r"D:\data-lake-project\data\user.csv", index=False)

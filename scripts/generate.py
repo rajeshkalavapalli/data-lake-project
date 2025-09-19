64ebe50eff0e301ddaaa0026671371ca5338ee37
rajesh <rajesh.kalavapalli>
2025-09-19 16:46:38 +0530

    data events and transection created

--- scripts/generate.py
+++ scripts/generate.py
@@ -23,3 +23,31 @@ df_users = pd.DataFrame(users)
 df_users.to_csv(r"D:\data-lake-project\data\user.csv", index=False)
 
 # genearete transection 
+transection = []
+start_date = datetime(2025,12, 5)
+
+for i in range(1,500):
+    transection.append({
+        'tsx_id':str(uuid.uuid4()),
+        'user_id':random.randint(1,100),
+        'amount':round(random.uniform(10,500),2),
+        'tsx_date':(start_date+timedelta(days=random.randint(0,365))).strftime("%Y-%m-%d")
+    })
+
+df_txn =pd.DataFrame(transection)
+df_txn.to_csv(r"D:\data-lake-project\data\transection.csv", index=False)
+
+# events generate 
+
+events = []
+events_date = datetime(2025, 4, 6)
+for i in range(1,300):
+    events.append({
+        'event_id':str(uuid.uuid4()),
+        'user_id':random.randint(1,100),
+        'event_type':random.choice(['click', 'view','purchase','addtocart']),
+        'event_time' : (start_date+timedelta(random.randint(0,365))).isoformat()
+    })
+
+df_event = pd.DataFrame(events)
+df_event.to_json(r"D:\data-lake-project\data\events.json", orient="records", lines=True)
